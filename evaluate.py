d1307833de7c5698ccfcb8151ff622b5bf526e1c
Barafat2 <basselarafat2@gmail.com>
2024-11-10 21:09:13 -0500

    real_data cleanup

--- evaluate.py
+++ evaluate.py
@@ -25,8 +25,8 @@ def U_MSE(U_true, U_pred):
     return np.mean(MSE)
 
 def center_normalize(X,axis=0):
-    X = X - X.mean(axis=axis)
-    X = X / np.linalg.norm(X, axis=axis)
+    X = X - X.mean(axis=axis, keepdims=True)
+    X = X / np.linalg.norm(X, axis=axis, keepdims=True)
     return X
 
 def get_U_hat_one_hot(U_hat):
@@ -102,8 +102,8 @@ def evaluate_dataframe_simulation(D, YLib,VLib, U_true,ytest,vtest,parcels_to_ev
     return D,Us
 
 
-def evaluate_single_real(combination, YLib,VLib,info,ytest, vtest,M_test,parcels_to_evaluate,estimation_method = 'hbp'):
-    ytest_run = pt.tensor(ytest_run,dtype=pt.float32)
+def evaluate_single_real(combination, YLib,VLib,info,ytest, vtest,M_test,ar_model,parcels_to_evaluate,estimation_method = 'hbp'):
+    ytest = pt.tensor(ytest,dtype=pt.float32)
     vtest = pt.tensor(vtest,dtype=pt.float32)
 
     # Get the task subset indices and corresponding data
@@ -114,43 +114,49 @@ def evaluate_single_real(combination, YLib,VLib,info,ytest, vtest,M_test,parcels
         V_subset = center_normalize(V_subset,axis=0)
 
         y_subset_run_1 = YLib[:, task_subset_indices, :]
-
-        run_2_indices = task_subset_indices * 2
+        n_tasks = VLib.shape[0]
+        run_2_indices = [(i + n_tasks) for i in task_subset_indices]
         y_subset_run_2 = YLib[:, run_2_indices, :]
 
         # y subset is the mean of the two runs
         y_subset = (y_subset_run_1 + y_subset_run_2) / 2
         y_subset = center_normalize(y_subset,axis=1)
-
-        U_hat = et.estimate_Us_projection(y_subset, V_subset)
-        U_hat_one_hot = get_U_hat_one_hot(U_hat)
+        
+        U_hats = []
+        for i in range(y_subset.shape[0]):
+            U_hat = et.estimate_Us_projection(y_subset[i], V_subset)
+            U_hats.append(U_hat)
+        U_hats = np.stack(U_hats)
+        U_hat_one_hot = get_U_hat_one_hot(U_hats)
         U_hat_evaluation = U_hat_one_hot[:,parcels_to_evaluate,:]
+        U_hat_evaluation = pt.tensor(U_hat_evaluation,dtype=pt.float32)
 
-        cos = hbpev.coserr(ytest,vtest,U_hat_evaluation,adjusted=False)
+        cos = hbpev.coserr(ytest,vtest,U_hat_evaluation,adjusted=True)
+        cos = cos.mean().item()
 
     elif estimation_method == 'hbp':
         # leverage repeats for HBP
         HBP_data,HBP_cond_vec,HBP_part_vec = ut.make_dataset(YLib,info,task_subset_indices,n_repeats=2)
-        U_hat_HBP = et.estiamte_HBP_U(HBP_data, HBP_cond_vec, HBP_part_vec)
+        HBP_data = center_normalize(HBP_data,axis=1)
+        U_hat_HBP = et.estiamte_HBP_U(HBP_data, HBP_cond_vec, HBP_part_vec,ar_model)
         U_hat_one_hot = get_U_hat_one_hot(U_hat_HBP)
         U_hat_HBP_eval = U_hat_HBP[:,parcels_to_evaluate,:]
         if U_hat_HBP_eval.ndim == 2:
             U_hat_HBP_eval = U_hat_HBP_eval.reshape(-1,1,U_hat_HBP_eval.shape[1])
         U_hat_HBP_eval = [U_hat_HBP_eval]
         # Compute cos_HBP
-        cos = calc_test_error(M=M_test, tdata=ytest_run, U_hats=U_hat_HBP_eval, fit_emission='use_Uhats').mean()
+        cos = calc_test_error(M=M_test, tdata=ytest, U_hats=U_hat_HBP_eval, fit_emission='use_Uhats').mean()
 
     
     return cos,U_hat_one_hot
 
 
-def evaluate_dataframe_real(D, YLib,VLib,info,ytest, vtest,M_test,parcels_to_evaluate,estimation_method = 'hbp'):
+def evaluate_dataframe_real(D, YLib,VLib,info,ytest, vtest,M_test,ar_model,parcels_to_evaluate,estimation_method = 'hbp'):
     # Create a new column with combinations as tuples to make them hashable
     D['combination_tuple'] = D['combination'].apply(lambda x: tuple(x)) 
     # Get unique combinations
     unique_combinations = D['combination_tuple'].unique()
 
-    U_true = U_true.numpy()
     Us = []
     cos_dict = {}
 
@@ -158,7 +164,7 @@ def evaluate_dataframe_real(D, YLib,VLib,info,ytest, vtest,M_test,parcels_to_eva
     for i, comb_tuple in enumerate(unique_combinations):
         if i % 100 == 0:
             print(f"Processing combination: {i}")
-        cos,U_hat_one_hot = evaluate_single_real(comb_tuple, YLib,VLib,info,ytest, vtest,M_test,parcels_to_evaluate,estimation_method = 'hbp')
+        cos,U_hat_one_hot = evaluate_single_real(comb_tuple, YLib,VLib,info,ytest, vtest,M_test,ar_model,parcels_to_evaluate,estimation_method = estimation_method)
         cos_dict[comb_tuple] = cos
         Us.append(U_hat_one_hot)
 
