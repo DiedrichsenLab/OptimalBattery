815ead39f143c10393715d20c56c7f187a3cba4b
Barafat2 <118376927+Barafat2@users.noreply.github.com>
2024-11-10 19:30:39 -0500

    simulations cleanup

--- estimate.py
+++ estimate.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 
-def estimate_Us_ols(Y,V,regularize = None):
+def estimate_Us_ols(Y,V):
     """
     get U_hat using OLS regression
     Args:
@@ -12,8 +12,17 @@ def estimate_Us_ols(Y,V,regularize = None):
         U_hat: Individual parcellations (n_subjects, n_components, n_voxels)
     """
     # Uhat =  (V^T V)^-1 V^T Y
-    if regularize is not None:
-        U_hats = np.linalg.inv(V.T @ V + regularize*np.eye(V.shape[1])) @ V.T @ Y
-    else:
-        U_hats = np.linalg.inv(V.T @ V) @ V.T @ Y
-    return U_hats # s, k, p
+    U_hats = np.linalg.inv(V.T @ V) @ V.T @ Y
+    return U_hats # k, p
+
+def estimate_Us_projection(Y,V):
+    """
+    get U_hat using projection
+    Args:
+        Y: Individual fMRI data (n_tasks, n_voxels)
+        V: Functional Profile (n_tasks, n_components)
+    Returns:
+        U_hat: Individual parcellations (n_components, n_voxels)
+    """
+    U_hats= V.T @ Y
+    return U_hats # k, p
--- evaluate.py
+++ evaluate.py
@@ -4,6 +4,9 @@ import torch as pt
 import OptimalBattery.simulate as sim
 import OptimalBattery.estimate as et
 import HierarchBayesParcel.evaluation as hbpev
+import OptimalBattery.util as ut
+from HierarchBayesParcel.evaluation import calc_test_error,coserr
+
 
 
 def U_MSE(U_true, U_pred):
@@ -21,18 +24,65 @@ def U_MSE(U_true, U_pred):
         MSE.append(mse)
     return np.mean(MSE)
 
+def center_normalize(X,axis=0):
+    X = X - X.mean(axis=axis)
+    X = X / np.linalg.norm(X, axis=axis)
+    return X
 
-def evaluate_combinations_simulation(D, YLib,VLib, U_true,ytest,vtest,parcels_to_evaluate,estimation_method = 'OLS'):
+def get_U_hat_one_hot(U_hat):
+    if len(U_hat.shape) == 2:
+        U_hat = U_hat.reshape(1, U_hat.shape[0], U_hat.shape[1])
+
+    Us=[]
+    for i in range(U_hat.shape[0]):
+        max_indices = np.argmax(U_hat[i], axis=0)
+        U_hat_one_hot = np.zeros_like(U_hat[i])
+        U_hat_one_hot[max_indices, np.arange(U_hat[i].shape[1])] = 1
+        Us.append(U_hat_one_hot)
+    Us = np.stack(Us)
+    return Us
+
+
+def evaluate_single_simulation(combination,YLib,VLib, U_true,ytest,vtest,parcels_to_evaluate,estimation_method = 'OLS'):
+    if type(U_true) == pt.Tensor:
+        U_true = U_true.numpy()
+    if len(U_true.shape) == 2:
+        U_true = U_true.reshape(1, U_true.shape[0], U_true.shape[1])
+        
+    # Get the task subset indices and corresponding data
+    task_subset_indices = list(combination)
+    # task_subset_indices = np.arange(25)
+    V_subset = VLib[task_subset_indices,:]
+    V_subset = center_normalize(V_subset,axis=0)
+
+    y_subset = YLib[task_subset_indices, :]
+    y_subset = center_normalize(y_subset,axis=0)
+
+    if estimation_method == 'OLS':
+        U_hat = et.estimate_Us_ols(y_subset, V_subset)
+        U_hat_one_hot = get_U_hat_one_hot(U_hat)
+    elif estimation_method == 'projection':
+        U_hat = et.estimate_Us_projection(y_subset, V_subset)
+        U_hat_one_hot = get_U_hat_one_hot(U_hat)
+
+    U_hat_evaluation = U_hat_one_hot[:,parcels_to_evaluate,:]
+    U_true_evaluation = U_true[:,parcels_to_evaluate,:]
+    mse = U_MSE(U_true_evaluation, U_hat_evaluation)
+
+    U_hat_evaluation = pt.tensor(U_hat_evaluation,dtype=pt.float32)
+    cos  =hbpev.coserr(ytest,vtest,U_hat_evaluation,adjusted=False)
+    cos = cos.item()
+    
+    return mse,cos,U_hat_one_hot
+
+
+def evaluate_dataframe_simulation(D, YLib,VLib, U_true,ytest,vtest,parcels_to_evaluate,estimation_method = 'OLS'):
     # Create a new column with combinations as tuples to make them hashable
     D['combination_tuple'] = D['combination'].apply(lambda x: tuple(x)) 
     # Get unique combinations
     unique_combinations = D['combination_tuple'].unique()
-    # Initialize a dictionary to store cos_HBP for each unique combination
-    mse_key = f"mse_{estimation_method.lower()}"
 
-    U_true = U_true.numpy()
     Us = []
-
     mse_dict = {}
     cos_dict = {}
 
@@ -40,83 +90,79 @@ def evaluate_combinations_simulation(D, YLib,VLib, U_true,ytest,vtest,parcels_to
     for i, comb_tuple in enumerate(unique_combinations):
         if i % 100 == 0:
             print(f"Processing combination: {i}")
-        
-        # Get the task subset indices and corresponding data
-        task_subset_indices = list(comb_tuple)
-        V_subset = VLib[task_subset_indices,:]
-        V_subset = V_subset - V_subset.mean(axis=0)
-        V_subset = V_subset / np.linalg.norm(V_subset, axis=0)
-
-
-        y_subset = YLib[task_subset_indices, :]
-        y_subset = y_subset - y_subset.mean(axis=0)
-        y_subset = y_subset / np.linalg.norm(y_subset, axis=0)
-        
-        if estimation_method == 'OLS':
-            U_hat = et.estimate_Us_ols(y_subset, V_subset,regularize=1e-4)
-            max_indices = np.argmax(U_hat, axis=0)
-            U_hat_one_hot = np.zeros_like(U_hat)
-            U_hat_one_hot[max_indices, np.arange(U_hat.shape[1])] = 1
-        elif estimation_method == 'projection':
-            U_hat = V_subset.T @ y_subset
-            max_indices = np.argmax(U_hat, axis=0)
-            U_hat_one_hot = np.zeros_like(U_hat)
-            U_hat_one_hot[max_indices, np.arange(U_hat.shape[1])] = 1
-        Us.append(U_hat_one_hot)
-
-        U_hat_evaluation = U_hat_one_hot[parcels_to_evaluate,:]
-        U_true_evaluation = U_true[parcels_to_evaluate,:]
-        mse = U_MSE(U_true_evaluation, U_hat_evaluation)
-
-        U_hat_evaluation = pt.tensor(U_hat_evaluation,dtype=pt.float32)
-        if len(U_hat_evaluation.shape) == 1:
-            U_hat_evaluation = U_hat_evaluation.reshape(1,U_hat_evaluation.shape[0])
-        cos  =hbpev.coserr(ytest,vtest,U_hat_evaluation,adjusted=False)
-        cos = cos.item()
-
+        mse,cos,U_hat_one_hot = evaluate_single_simulation(comb_tuple,YLib,VLib, U_true,ytest,vtest,parcels_to_evaluate,estimation_method)
         mse_dict[comb_tuple] = mse
         cos_dict[comb_tuple] = cos
+        Us.append(U_hat_one_hot)
+
     
     # Map the computed cos_HBP values back to the DataFrame
-    D[mse_key] = D['combination_tuple'].map(mse_dict)    
+    D['mse'] = D['combination_tuple'].map(mse_dict)    
     D['cos'] = D['combination_tuple'].map(cos_dict)
     return D,Us
 
-def evaluate_single_combination(YLib,VLib, U_true,combination,ytest,vtest,parcels_to_evaluate,estimation_method = 'OLS'):
-    U_true = U_true.numpy()
-        
+
+def evaluate_single_real(combination, YLib,VLib,info,ytest, vtest,M_test,parcels_to_evaluate,estimation_method = 'hbp'):
+    ytest_run = pt.tensor(ytest_run,dtype=pt.float32)
+    vtest = pt.tensor(vtest,dtype=pt.float32)
+
     # Get the task subset indices and corresponding data
     task_subset_indices = list(combination)
-    # task_subset_indices = np.arange(25)
-    V_subset = VLib[task_subset_indices,:]
-    V_subset = V_subset - V_subset.mean(axis=0)
-    V_subset = V_subset / np.linalg.norm(V_subset, axis=0)
 
-    y_subset = YLib[task_subset_indices, :]
-    y_subset = y_subset - y_subset.mean(axis=0)
-    y_subset = y_subset / np.linalg.norm(y_subset, axis=0)
+    if estimation_method == 'projection':
+        V_subset = VLib[task_subset_indices, :]
+        V_subset = center_normalize(V_subset,axis=0)
 
-    if estimation_method == 'OLS':
-        U_hat = et.estimate_Us_ols(y_subset, V_subset,regularize=1e-4)
-        max_indices = np.argmax(U_hat, axis=0)
-        U_hat_one_hot = np.zeros_like(U_hat)
-        U_hat_one_hot[max_indices, np.arange(U_hat.shape[1])] = 1
-    elif estimation_method == 'projection':
-        U_hat = V_subset.T @ y_subset
-        max_indices = np.argmax(U_hat, axis=0)
-        U_hat_one_hot = np.zeros_like(U_hat)
-        U_hat_one_hot[max_indices, np.arange(U_hat.shape[1])] = 1
+        y_subset_run_1 = YLib[:, task_subset_indices, :]
 
-    U_hat_evaluation = U_hat_one_hot[parcels_to_evaluate,:]
-    U_true_evaluation = U_true[parcels_to_evaluate,:]
-    mse = U_MSE(U_true_evaluation, U_hat_evaluation)
+        run_2_indices = task_subset_indices * 2
+        y_subset_run_2 = YLib[:, run_2_indices, :]
 
-    U_hat_evaluation = pt.tensor(U_hat_evaluation,dtype=pt.float32)
-    if len(U_hat_evaluation.shape) == 1:
-        U_hat_evaluation = U_hat_evaluation.reshape(1,U_hat_evaluation.shape[0])
-    cos  =hbpev.coserr(ytest,vtest,U_hat_evaluation,adjusted=False)
-    cos = cos.item()
+        # y subset is the mean of the two runs
+        y_subset = (y_subset_run_1 + y_subset_run_2) / 2
+        y_subset = center_normalize(y_subset,axis=1)
+
+        U_hat = et.estimate_Us_projection(y_subset, V_subset)
+        U_hat_one_hot = get_U_hat_one_hot(U_hat)
+        U_hat_evaluation = U_hat_one_hot[:,parcels_to_evaluate,:]
+
+        cos = hbpev.coserr(ytest,vtest,U_hat_evaluation,adjusted=False)
+
+    elif estimation_method == 'hbp':
+        # leverage repeats for HBP
+        HBP_data,HBP_cond_vec,HBP_part_vec = ut.make_dataset(YLib,info,task_subset_indices,n_repeats=2)
+        U_hat_HBP = et.estiamte_HBP_U(HBP_data, HBP_cond_vec, HBP_part_vec)
+        U_hat_one_hot = get_U_hat_one_hot(U_hat_HBP)
+        U_hat_HBP_eval = U_hat_HBP[:,parcels_to_evaluate,:]
+        if U_hat_HBP_eval.ndim == 2:
+            U_hat_HBP_eval = U_hat_HBP_eval.reshape(-1,1,U_hat_HBP_eval.shape[1])
+        U_hat_HBP_eval = [U_hat_HBP_eval]
+        # Compute cos_HBP
+        cos = calc_test_error(M=M_test, tdata=ytest_run, U_hats=U_hat_HBP_eval, fit_emission='use_Uhats').mean()
+
     
-    return mse,cos,U_hat_one_hot
+    return cos,U_hat_one_hot
 
 
+def evaluate_dataframe_real(D, YLib,VLib,info,ytest, vtest,M_test,parcels_to_evaluate,estimation_method = 'hbp'):
+    # Create a new column with combinations as tuples to make them hashable
+    D['combination_tuple'] = D['combination'].apply(lambda x: tuple(x)) 
+    # Get unique combinations
+    unique_combinations = D['combination_tuple'].unique()
+
+    U_true = U_true.numpy()
+    Us = []
+    cos_dict = {}
+
+    # Loop over each unique combination
+    for i, comb_tuple in enumerate(unique_combinations):
+        if i % 100 == 0:
+            print(f"Processing combination: {i}")
+        cos,U_hat_one_hot = evaluate_single_real(comb_tuple, YLib,VLib,info,ytest, vtest,M_test,parcels_to_evaluate,estimation_method = 'hbp')
+        cos_dict[comb_tuple] = cos
+        Us.append(U_hat_one_hot)
+
+    
+    # Map the computed cos values back to the DataFrame
+    D['cos'] = D['combination_tuple'].map(cos_dict)
+    return D,Us
