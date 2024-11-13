bf485a7f389f8f03a0135b8641bee98a8cba9775
Bassel Arafat <118376927+Barafat2@users.noreply.github.com>
2024-11-13 11:14:52 -0500

    parcellation simulation

--- evaluate.py
+++ evaluate.py
@@ -30,35 +30,39 @@ def center_normalize(X,axis=0):
     return X
 
 def get_U_hat_one_hot(U_hat):
-    if len(U_hat.shape) == 2:
-        U_hat = U_hat.reshape(1, U_hat.shape[0], U_hat.shape[1])
-    if type(U_hat) == pt.Tensor:
-        U_hat = U_hat.cpu().numpy()
+    max_indices = np.argmax(U_hat, axis=0)
+    U_hat_one_hot = np.zeros_like(U_hat)
+    U_hat_one_hot[max_indices, np.arange(U_hat.shape[1])] = 1
+    return U_hat_one_hot
 
-    Us=[]
-    for i in range(U_hat.shape[0]):
-        max_indices = np.argmax(U_hat[i], axis=0)
-        U_hat_one_hot = np.zeros_like(U_hat[i])
-        U_hat_one_hot[max_indices, np.arange(U_hat[i].shape[1])] = 1
-        Us.append(U_hat_one_hot)
-    Us = np.stack(Us)
-    return Us
+def percentage_correct(U_true, U_pred):
+    correct_voxels = np.sum(U_true * U_pred)
+    total_voxels = U_true.shape[1]
+    
+    percentage = (correct_voxels / total_voxels) * 100
+    return percentage
 
+def prediction_error(ytest,vtest,U_hat):
+    yhat = np.matmul(vtest,U_hat)
+    cosine_error_vox = 1 - np.sum(ytest * yhat,axis = 0)
+    cos_err = np.mean(cosine_error_vox)
+    return cos_err
 
-def evaluate_single_simulation(combination,YLib,VLib, U_true,ytest,vtest,parcels_to_evaluate,estimation_method = 'OLS'):
-    if type(U_true) == pt.Tensor:
-        U_true = U_true.numpy()
-    if len(U_true.shape) == 2:
-        U_true = U_true.reshape(1, U_true.shape[0], U_true.shape[1])
-        
+def evaluate_single_simulation_parcellation(combination,YLib,VLib, U_true,estimation_method = 'OLS'):
     # Get the task subset indices and corresponding data
     task_subset_indices = list(combination)
-    # task_subset_indices = np.arange(25)
+
     V_subset = VLib[task_subset_indices,:]
     V_subset = center_normalize(V_subset,axis=0)
 
     y_subset = YLib[task_subset_indices, :]
+    y_subset = y_subset + np.random.normal(0, 0.04, y_subset.shape)
     y_subset = center_normalize(y_subset,axis=0)
+
+    ytest = YLib + np.random.normal(0, 0.04, YLib.shape)
+    ytest = center_normalize(ytest,axis=0)
+    vtest = VLib
+    vtest = center_normalize(vtest,axis=0)
 
     if estimation_method == 'OLS':
         U_hat = et.estimate_Us_ols(y_subset, V_subset)
@@ -66,40 +70,36 @@ def evaluate_single_simulation(combination,YLib,VLib, U_true,ytest,vtest,parcels
     elif estimation_method == 'projection':
         U_hat = et.estimate_Us_projection(y_subset, V_subset)
         U_hat_one_hot = get_U_hat_one_hot(U_hat)
-
-    U_hat_evaluation = U_hat_one_hot[:,parcels_to_evaluate,:]
-    U_true_evaluation = U_true[:,parcels_to_evaluate,:]
-    mse = U_MSE(U_true_evaluation, U_hat_evaluation)
-
-    U_hat_evaluation = pt.tensor(U_hat_evaluation,dtype=pt.float32)
-    cos  =hbpev.coserr(ytest,vtest,U_hat_evaluation,adjusted=False)
-    cos = cos.item()
     
-    return mse,cos,U_hat_one_hot
+    #eval
+    perc = percentage_correct(U_true=U_true, U_pred=U_hat_one_hot)
+    cos  = prediction_error(ytest,vtest,U_hat_one_hot)
+    
+    return perc,cos,U_hat_one_hot
 
 
-def evaluate_dataframe_simulation(D, YLib,VLib, U_true,ytest,vtest,parcels_to_evaluate,estimation_method = 'OLS'):
+def evaluate_dataframe_simulation_parcellation(D, YLib,VLib, U_true,estimation_method = 'OLS'):
     # Create a new column with combinations as tuples to make them hashable
     D['combination_tuple'] = D['combination'].apply(lambda x: tuple(x)) 
     # Get unique combinations
     unique_combinations = D['combination_tuple'].unique()
 
     Us = []
-    mse_dict = {}
+    perc_correct= {}
     cos_dict = {}
 
     # Loop over each unique combination
     for i, comb_tuple in enumerate(unique_combinations):
-        if i % 100 == 0:
+        if i % 1000 == 0:
             print(f"Processing combination: {i}")
-        mse,cos,U_hat_one_hot = evaluate_single_simulation(comb_tuple,YLib,VLib, U_true,ytest,vtest,parcels_to_evaluate,estimation_method)
-        mse_dict[comb_tuple] = mse
+        perc,cos,U_hat_one_hot = evaluate_single_simulation_parcellation(comb_tuple,YLib,VLib, U_true,estimation_method)
+        perc_correct[comb_tuple] = perc
         cos_dict[comb_tuple] = cos
         Us.append(U_hat_one_hot)
 
     
     # Map the computed cos_HBP values back to the DataFrame
-    D['mse'] = D['combination_tuple'].map(mse_dict)    
+    D['perc'] = D['combination_tuple'].map(perc_correct)    
     D['cos'] = D['combination_tuple'].map(cos_dict)
     return D,Us
 
